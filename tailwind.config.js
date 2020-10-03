2f9b3a10b912ce0cd0c8b9e7a2efdd91fb87fa92
FredPeal <frederickpeal@gmail.com>
2020-10-02 21:40:41 -0400

    Working with cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,24 @@ module.exports = {
   purge: [],
   theme: {
     extend: {},
+    fontFamily: {
+      'body': ['Spartan']
+    },
+    textColor: {
+      'dark-magneta': '#502050',
+      'soft-pink': '#ee68a4',
+      'dark-grayish-magenta': '#937b92',
+      'light-grayish-magenta': '#f7f2f7',
+      'white': '#ffffff'
+    },
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'light-grayish-magenta': '#f7f2f7',
+      'dark-magneta': '#502050'
+    })
+    // fontSize: {
+    //   base: ['15px'],
+    // }
   },
   variants: {},
   plugins: [],
